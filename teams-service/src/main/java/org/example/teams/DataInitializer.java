ab698f9ded64f272869b3ea1c456f5859607b330
wikto <wiktorbanach123@gmail.com>
2026-02-16 13:26:15 +0100

    lab6

--- teams-service/src/main/java/org/example/teams/DataInitializer.java
+++ teams-service/src/main/java/org/example/teams/DataInitializer.java
@@ -14,8 +14,8 @@ public class DataInitializer implements CommandLineRunner {
 
     @Override
     public void run(String... args) {
-        Team g2 = new Team(UUID.randomUUID(), "G2 Esports", "Europe", 2013);
-        Team t1 = new Team(UUID.randomUUID(), "T1", "Korea", 2003);
+        Team g2 = new Team(UUID.fromString("07ac0630-93f2-4acd-bbb2-d2e29cfa062a"), "G2 Esports", "Europe", 2013);
+        Team t1 = new Team(UUID.fromString("6af0b5cb-dee2-44f9-9adb-bbd42c41d90c"), "T1", "Korea", 2003);
 
         teamService.save(g2);
         teamService.save(t1);
